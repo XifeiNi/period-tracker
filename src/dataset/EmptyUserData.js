73b6461c4d4dc953f5348254d51eb627547a6545
Jack Jiang <jackjyq@outlook.com>
2019-07-27 22:34:59 +1000

    change data format

--- src/dataset/EmptyUserData.js
+++ src/dataset/EmptyUserData.js
@@ -3,12 +3,14 @@
     "Pain": 0, 1, 2, 3, 4
     "Mood": 0, 1, 2, 3, 4
     "Sex": 0, 1, 2
+    "Mental": 0, 1, 2, 3, 4
 */
 const EmptyUserDate = {
     "Blood": 0,
     "Pain": 0,
     "Mood": 0,
-    "Sex": 0
+    "Sex": 0,
+    "Mental": 0
 }
 
 export default EmptyUserDate;
