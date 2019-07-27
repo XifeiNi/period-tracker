5ee3e2446de63faba69a0573819408de8dd47ea3
0xlightyear <0xlightyear@gmail.com>
2019-07-28 02:03:20 +1000

    state transfer done

--- src/Add.js
+++ src/Add.js
@@ -9,6 +9,21 @@ import b1 from './ICON/bleed/1.png'
 import b2 from './ICON/bleed/2.png'
 import b3 from './ICON/bleed/3.png'
 import b4 from './ICON/bleed/4.png'
+import m1 from './ICON/mood/1.png'
+import m2 from './ICON/mood/2.png'
+import m3 from './ICON/mood/3.png'
+import m4 from './ICON/mood/4.png'
+import p1 from './ICON/pain/1.png'
+import p2 from './ICON/pain/2.png'
+import p3 from './ICON/pain/3.png'
+import p4 from './ICON/pain/4.png'
+import s1 from './ICON/sex/1.png'
+import s2 from './ICON/sex/2.png'
+import bt from './ICON/bleed/theme.png'
+import mt from './ICON/mood/theme.png'
+import pt from './ICON/pain/theme.png'
+import st from './ICON/sex/theme.png'
+
 import EmptyUserData from './dataset/EmptyUserData';
 import homebackground from './ICON/background/background.png';
 
@@ -18,12 +33,25 @@ class Add extends React.Component{
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.state = {"currentPage": "blood"
-                 };
+    this.state = {"currentPage": "blood"};
   }
 
-  nextState(state){
-    this.setState(state);
+  nextState = () => {
+    let state = this.state;
+    if (state["currentPage"] === "blood"){
+      this.setState({currentPage: "mood"});
+    }
+    else if (state["currentPage"] === "mood"){
+      this.setState({currentPage: "pain"});
+    }
+    else if (state["currentPage"] === "pain"){
+      this.setState({currentPage: "sex"});
+    }
+    else if (state["currentPage"] === "sex"){
+      let state = this.props.state;
+      state["currentPage"] = "Home";
+      this.props.handleChangeState(state);
+    }
   }
 
   handleClick(e) {
@@ -33,6 +61,7 @@ class Add extends React.Component{
   }
 
   render() {
+    let state = this.state;
     const useStyles = makeStyles(theme => ({
       root: {
         flexGrow: 1,
@@ -44,7 +73,8 @@ class Add extends React.Component{
         margin: theme.spacing(1),
       }
     }));
-
+    console.log(state["currentPage"]);
+    if (state["currentPage"] === "blood"){
     return (
       <div>
         <div className={useStyles.root}>
@@ -60,18 +90,98 @@ class Add extends React.Component{
           </AppBar>
         </div>
         <div className="head">
-          <img className="image" alt="404" src={ testImg } />
+          <img className="image" alt="404" src={ bt } />
         </div>
         <div className="bleed_choice">
-        <img className="image" alt="404" src={ b1 } onClick={this.handleClick}/>
-        <img className="image" alt="404" src={ b2 } onClick={this.handleClick}/>
+        <img className="image" alt="404" src={ b1 } onClick={this.nextState}/>
+        <img className="image" alt="404" src={ b2 } onClick={this.nextState}/>
         </div>
         <div className="bleed_choice">
-        <img className="image" alt="404" src={ b3 } onClick={this.handleClick}/>
-        <img className="image" alt="404" src={ b4 } onClick={this.handleClick}/>
+        <img className="image" alt="404" src={ b3 } onClick={this.nextState}/>
+        <img className="image" alt="404" src={ b4 } onClick={this.nextState}/>
         </div>
         </div>
-    );
+    )}
+    else if (state["currentPage"] === "mood"){
+      return (
+        <div>
+          <div className={useStyles.root}>
+            <AppBar position="static" className="app-bar">
+              <Toolbar>
+                <Button variant="contained" color="primary" onClick={this.handleClick}>
+                X
+                </Button>
+                <Button variant="contained" color="primary" onClick={this.handleClick}>
+                  Done
+                </Button>
+              </Toolbar>
+            </AppBar>
+          </div>
+          <div className="head">
+            <img className="image" alt="404" src={ mt } />
+          </div>
+          <div className="bleed_choice">
+          <img className="image" alt="404" src={ m1 } onClick={this.nextState}/>
+          <img className="image" alt="404" src={ m2 } onClick={this.nextState}/>
+          </div>
+          <div className="bleed_choice">
+          <img className="image" alt="404" src={ m3 } onClick={this.nextState}/>
+          <img className="image" alt="404" src={ m4 } onClick={this.nextState}/>
+          </div>
+          </div>
+      )}
+    else if (state["currentPage"] === "pain"){
+      return (
+        <div>
+          <div className={useStyles.root}>
+            <AppBar position="static" className="app-bar">
+              <Toolbar>
+                <Button variant="contained" color="primary" onClick={this.handleClick}>
+                X
+                </Button>
+                <Button variant="contained" color="primary" onClick={this.handleClick}>
+                  Done
+                </Button>
+              </Toolbar>
+            </AppBar>
+          </div>
+          <div className="head">
+            <img className="image" alt="404" src={ pt } />
+          </div>
+          <div className="bleed_choice">
+          <img className="image" alt="404" src={ p1 } onClick={this.nextState}/>
+          <img className="image" alt="404" src={ p2 } onClick={this.nextState}/>
+          </div>
+          <div className="bleed_choice">
+          <img className="image" alt="404" src={ p3 } onClick={this.nextState}/>
+          <img className="image" alt="404" src={ p4 } onClick={this.nextState}/>
+          </div>
+          </div>
+      )}
+      else if (state["currentPage"] === "sex"){
+        return (
+          <div>
+            <div className={useStyles.root}>
+              <AppBar position="static" className="app-bar">
+                <Toolbar>
+                  <Button variant="contained" color="primary" onClick={this.handleClick}>
+                  X
+                  </Button>
+                  <Button variant="contained" color="primary" onClick={this.handleClick}>
+                    Done
+                  </Button>
+                </Toolbar>
+              </AppBar>
+            </div>
+            <div className="head">
+              <img className="image" alt="404" src={ st } />
+            </div>
+            <div className="bleed_choice">
+            <img className="image" alt="404" src={ s1 } onClick={this.nextState}/>
+            <img className="image" alt="404" src={ s2 } onClick={this.nextState}/>
+            </div>
+            </div>
+        )}
   }
 }
 export default Add;
