ba4db55b5fc3ceef5005474d80b41d205c438fdb
Jack Jiang <jackjyq@outlook.com>
2019-07-28 10:36:54 +1000

    change algorithm

--- src/CoreAlgorithm.js
+++ src/CoreAlgorithm.js
@@ -2,7 +2,7 @@ function Predict(state) {
     // console.log(state);
     const userData = state["userData"];
     // console.log(userData);
-    for (let date in Object.keys(userData).ScreenOrientation()) {
+    for (let date in Object.keys(userData).sort()) {
         console.log(date);
     }
 }
